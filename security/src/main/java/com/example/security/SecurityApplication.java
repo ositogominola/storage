89ab899983f146d5a8c802f9ed6fe6e76b46a534
ositogominola <hanertovary@gmail.com>
2023-01-24 20:28:19 -0500

    jwt authentication

--- security/src/main/java/com/example/security/SecurityApplication.java
+++ security/src/main/java/com/example/security/SecurityApplication.java
@@ -1,9 +1,12 @@
 package com.example.security;
 
+import com.example.security.conffig.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class SecurityApplication {
 
 	public static void main(String[] args) {
---
+++ security/src/main/java/com/example/security/models/user.java
@@ -0,0 +1,49 @@
+package com.example.security.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user")
+public class user {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUser;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false, unique = true)
+
+    private String username;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Boolean enabled;
+
+    @Column(nullable = true)
+    private String roles;
+
+    public boolean is_valid(){
+        if (this.name==null || this.lastname==null || this.username==null || this.email==null || this.password==null || this.age==null ||this.name=="" || this.lastname=="" || this.username=="" || this.email=="" || this.password=="" ||  this.age<=0){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+}
