743839763efef676e298af0459cec4436bf33905
ositogominola <hanertovary@gmail.com>
2023-03-25 11:42:30 -0500

    ultimo paso, terminacion de servicios y coneccion de estos

---
+++ gateway/Routers/userRouters.py
@@ -0,0 +1,63 @@
+from flask import Blueprint, request
+import requests
+from settings.loadFileConfig import loadFileConfig
+
+user = Blueprint("user", __name__)
+dataConfig = loadFileConfig()
+
+@user.route("/create_user", methods=["POST"])
+def create_user():
+    user=request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url=dataConfig["url-backend-security"]+"/user/create"
+    return requests.post(url, headers=headers, json=user).json()
+
+@user.route("/login", methods=["POST", "GET"])
+def login():
+    headers = {
+        'Authorization': request.headers.get('Authorization'),
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    url = dataConfig["url-backend-security"] + "/login"
+    token=requests.post(url, headers=headers)
+    if token.status_code==200:
+        return token.text
+    return "credenciales incorrectas"
+
+@user.route("/addPermission/<id>/idrol/<idrol>", methods=["POST"])
+def addPermision(id, idrol):
+    headers = {
+        'Authorization': request.headers.get('Authorization'),
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    url = dataConfig["url-backend-security"] +"/user/addpermission/"+id+"/rol/"+idrol
+    return requests.post(url,headers=headers).json()
+
+@user.route("/create_role", methods=["POST"])
+def createRol():
+    rol=request.get_json()
+    headers = {
+        'Authorization': request.headers.get('Authorization'),
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    url=dataConfig["url-backend-security"] +"/user/createRole"
+    return requests.post(url,headers=headers, json=rol).json()
+
+@user.route("/create_permission", methods=["POST"])
+def createPermission():
+    rol=request.get_json()
+    headers = {
+        'Authorization': request.headers.get('Authorization'),
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    url=dataConfig["url-backend-security"] +"/user/createPermissions"
+    return requests.post(url,headers=headers, json=rol).json()
+
+@user.route("/addRol/<idrol>/us/<idus>", methods=["PUT"])
+def addRol(idrol, idus):
+    headers = {
+        'Authorization': request.headers.get('Authorization'),
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    url=dataConfig["url-backend-security"] +"/user/rol/"+idrol+"/us/"+idus
+    return requests.put(url, headers=headers).json()
